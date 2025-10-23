067ca8535b0dd7eaf990b40f4e9f864cfb4ce9d1
onguerrerov <onguerrerov@uncg.edu>
2025-10-23 15:07:30 -0400

    assignment submission

--- code/assignment.R
+++ code/assignment.R
@@ -10,42 +10,64 @@ pacman::p_load(tidyverse,
 # Q1. Read the dataset "data_finsync_nc.csv" from the data subdirectory 
 # and assign it to an object named `df_fish`.
 # Reference: Chapter 2
+(df_fish <- read_csv(here("data/data_finsync_nc.csv")))
 
 # Q2. Using `df_fish`, create a dataframe with unique site name (`site_id`), 
 # longitude (`lon`), and latitude (`lat`), and assign it to `df_site`.
 # Hint: use `distinct()` function.
 # Reference: Chapter 2
+df_site <- df_fish %>%
+  distinct(site_id,
+           lon,
+           lat) %>%
+  st_as_sf(coords = c("lon", "lat"))
 
 # Q3. `df_site` currently has no coordinate reference system (CRS). 
 # Convert it to an `sf` object and assign the WGS 84 CRS (EPSG: 4326). 
 # Save the resulting object as `sf_site`.
 # Reference: Chapter 2
+sf_site <- df_site %>%
+  st_as_sf(coords = c("long", "lat"),
+           crs = 4326)
 
 # Q4. Read "sf_nc_county.rds" from the data subdirectory using the `readRDS()` function.
 # Assign the result to `sf_nc_county`.
 # Reference: Chapter 3
+(sf_nc_county <- readRDS(file = here("data/sf_nc_county.rds")))
 
 # Q5. Using the `st_join()` function, associate the county column from `sf_nc_county`
 # with each site in `sf_site`. Assign the result to `sf_site_w_county`.
 # Reference: Chapter 3
+sf_site_w_county <- st_join(sf_site, sf_nc_county)
 
 # Q6. Convert `sf_site_w_county` to a tibble using the `as_tibble()` function.
 # Assign the result to `df_site_w_county`.
 # Reference: Chapter 3
+df_site_w_county <- as_tibble(sf_site_w_county)
 
 # Q7. Using `df_site_w_county`, create a vector of county names that have at least one fish survey site.
 # Assign the result to `v_s1`. (Hint: use group_by(), summarize(), filter(), and pull()).
 # Confirm the result contains 49 counties with `length(v_s1)`.
 # Reference: https://aterui.github.io/biostats/data-manipulation.html#group-operation
+v_s1 <- df_site_w_county %>%
+  group_by(county) %>%
+  summarize(site_count = n()) %>%
+  filter(site_count >= 1) %>%
+  pull(county)
 
+length(v_s1)
 # Q8. Using `v_s1`, subset the county polygons in `sf_nc_county` 
 # to include only counties with at least one site.
 # Assign the result to `sf_county_s1`.
 # Reference: https://aterui.github.io/biostats/data-manipulation.html#row-manipulation
+sf_county_s1 <- st_filter(sf_nc_county, v_s1,)
 
 # Q9. Display `sf_county_s1` along with all sampling sites (`sf_site`) 
 # on a single map using `ggplot()` and `geom_sf()`.
 # Reference: Chapter 3
+ggplot() +
+  geom_sf(data = sf_county_s1) +
+  geom_sf(data = sf_site)
 
 # Q10. Among the counties that contain at least one sampling site, 
 # calculate the area of each county polygon using `st_area()`. 
